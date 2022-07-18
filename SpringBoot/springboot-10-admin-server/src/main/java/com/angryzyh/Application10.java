7d7c45d66d78047d887324c49521a5d65abdfe62
angryzyh <1792090548@qq.com>
2022-07-18 22:47:46 +0800

    26th commit SBMP框架 springboot-admin监控

---
+++ SpringBoot/springboot-10-admin-server/src/main/java/com/angryzyh/Application10.java
@@ -0,0 +1,15 @@
+package com.angryzyh;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Application10 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application10.class, args);
+    }
+
+}
---
+++ SpringBoot/springboot-11-admin-client/src/main/java/com/angryzyh/Application11.java
@@ -0,0 +1,13 @@
+package com.angryzyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application11 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application11.class, args);
+    }
+
+}
